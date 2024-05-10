d1cfacfb321daf673d5d26afdeb040382c44e4ff
damlaalim <damlaalim81@gmail.com>
2024-05-11 00:35:33 +0300

    added player movement

---
+++ Assets/_GoatJam24/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+
+namespace _GoatJam24.Scripts.Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+
+        private void Update()
+        {
+            var moveHz = Input.GetAxis("Horizontal");
+            var moveVt = Input.GetAxis("Vertical");
+
+            var movement = new Vector3(moveHz, moveVt, 0) * _speed ;
+
+            transform.position += movement;
+        }
+    }
+}
